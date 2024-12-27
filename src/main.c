294f99dc9a54ef1c05646e707ec1e36f951b5aa7
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-12-27 13:32:03 +0400

    feat: initializing and saving a blank scene

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
+#include "bitmap.h"
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WIDTH 1280
+#define HEIGHT 720
 
 int main() {
-    printf("Hello, world!\n");
+    // Seed rng
+    srand(time(NULL));
+
+    // Initialize frame and fill it with black
+    uint8_t frame[WIDTH * HEIGHT][3];
+    for (int i = 0; i < WIDTH * HEIGHT; i++) {
+        frame[i][0] = 0;
+        frame[i][1] = 0;
+        frame[i][2] = 0;
+    }
+
+    write_bitmap("output.bmp", WIDTH, HEIGHT, frame);
     return 0;
 }
