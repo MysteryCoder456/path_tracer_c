c29ec5c142adc03a396f9751c64d302f618cfb43
MysteryCoder456 <rehatbir.singh@gmail.com>
2025-02-08 10:48:09 -0600

    feat: ray-sphere and ray-triangle intersections

--- shaders/rtx_frag.glsl
+++ shaders/rtx_frag.glsl
@@ -50,11 +50,59 @@ uniform int triangle_count;
 float tan_fov_2;
 
 float ray_sphere_intersect(vec3 o, vec3 d, Sphere s) {
-    // TODO: ray-sphere intersection
+    // Translate ray so that sphere is at origin
+    o -= s.center;
+
+    // Quadratic equation
+    float a = dot(d, d);
+    float b = 2.0 * (o.x * d.x + o.y * d.y + o.z * d.z);
+    float c = dot(o, o) - pow(s.radius, 2.0);
+    float det = b * b - 4.0 * a * c;
+
+    // Equation has no real solutions
+    if (det < 0)
+        return -1;
+
+    float t1 = (-b - sqrt(det)) / 2.0 * a;
+    if (t1 >= 0)
+        return t1;
+
+    float t2 = (-b + sqrt(det)) / 2.0 * a;
+    if (t2 >= 0)
+        return t2;
+
+    return -1;
 }
 
-float ray_triangle_intersect(vec3 o, vec3 d, Triangle t) {
-    // TODO: ray-triangle intersection
+// Moller-Trumbore algorithm
+// Adapted from https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm#C++_implementation
+float ray_triangle_intersect(vec3 o, vec3 d, Triangle tr) {
+    const float epsilon = 1e-6;
+
+    vec3 edge1 = tr.v1 - tr.v0;
+    vec3 edge2 = tr.v2 - tr.v0;
+    vec3 ray_cross_e2 = cross(d, edge2);
+    float det = dot(edge1, ray_cross_e2);
+
+    if (det > -epsilon && det < epsilon)
+        return -1;
+
+    float inv_det = 1.0 / det;
+    vec3 s = o - tr.v0;
+    float u = inv_det * dot(s, ray_cross_e2);
+
+    if ((u < 0 && abs(u) > epsilon) || (u > 1 && abs(u - 1) > epsilon))
+        return -1;
+
+    vec3 s_cross_e1 = cross(s, edge1);
+    float v = inv_det * dot(d, s_cross_e1);
+
+    if ((v < 0 && abs(v) > epsilon) || (u + v > 1 && abs(u + v - 1) > epsilon))
+        return -1;
+
+    // At this stage we can compute t to find out where the intersection point is on the line.
+    float t = inv_det * dot(edge2, s_cross_e1);
+    return t;
 }
 
 RayHit trace_ray(vec3 origin, vec3 direction) {
